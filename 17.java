6125e5c37f0bfdd573b4715cdc205458a48b803d
IvanKologrivenko <57537507+IvanKologrivenko@users.noreply.github.com>
2019-11-17 17:33:48 +0200

    Create 17.java

---
+++ 17.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (int i = 102; i <= 200; i += 17) {
+            System.out.println("number is:" + i);
+        }
+    }
+}
