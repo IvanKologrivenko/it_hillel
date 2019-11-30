a1692d8cfd712851bc3647e3ee99d2192d10021f
IvanKologrivenko <57537507+IvanKologrivenko@users.noreply.github.com>
2019-11-30 02:00:11 +0200

    Update separator.java

--- separator.java
+++ separator.java
@@ -2,15 +2,17 @@ package com.company;
 
 public class Main {
 
+    private static final int INT = 100;
+
     public static void main(String[] args) {
         printLine( "++");
         System.out.println(printLine("++"));
     }
     private static String printLine(String separator) {
         StringBuilder sb = new StringBuilder();
-        for (int i = 1; i <= 100; i++) {
+        for (int i = 1; i <= INT; i++) {
             sb.append(i);
-            if (i != 100) {
+            if (i != INT) {
                 sb.append(separator);
             }
         }
